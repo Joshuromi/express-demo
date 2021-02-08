8acaf7d1b876f6523925b199b3ceaa3703733d3b
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-02-08 21:54:38 +0100

    installed morgan and set it to run on only development serevr

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const Joi = require("joi");
 const express = require("express");
+const Joi = require("joi");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(helmet());
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Mongan enabled...");
+}
 
 const courses = [
   { id: 1, name: "course 1" },
