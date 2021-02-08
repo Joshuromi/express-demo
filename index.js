c72706712e695e5c03718f2c3643d291a0b9eeaa
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-02-08 20:10:13 +0100

    added delete route

--- index.js
+++ index.js
@@ -22,38 +22,16 @@ app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(
     (course) => course.id === parseInt(req.params.id)
   );
-  if (!course) res.status(404).send("The course with this id was not found.");
-  else res.send(course);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find(
-    (course) => course.id === parseInt(req.params.id)
-  );
   if (!course) {
-    res.status(404).send("The course with this id was not found.");
+    return res.status(404).send("The course with this id was not found.");
   }
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-  });
-  const result = schema.validate(req.body);
-  if (result.error) {
-    res.status(400).send(result.error.message);
-    return;
-  }
-
-  course.name = req.body.name;
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-  });
-  const result = schema.validate(req.body);
-  if (result.error) {
-    res.status(400).send(result.error.message);
-    return;
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.message);
   }
 
   const course = {
@@ -63,6 +41,43 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (!course) {
+    return res.status(404).send("The course with this id was not found.");
+  }
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.message);
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (!course) {
+    return res.status(404).send("The course with this id was not found.");
+  }
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+  });
+  return schema.validate(course);
+}
 
 const port = process.env.PORT || 3000;
 
---
+++ logger.js
@@ -0,0 +1,6 @@
+function logger(req, res, next) {
+  console.log();
+  next();
+}
+
+module.exports = logger;
